5e7a5ed83e650fe70067227c1af01a66abe83074
Andy Liang <meteochu@gmail.com>
2019-09-02 17:26:59 -0700

    Add basic Package.swift supporting SPM

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "IceCream",
+    platforms: [
+        .iOS(.v10), .tvOS(.v10), .watchOS(.v3), .macOS(.v10_12)
+    ],
+    products: [
+        .library(name: "IceCream", targets: ["IceCream"]),
+    ],
+    dependencies: [
+        .package(url: "git@github.com:realm/realm-cocoa.git", from: "3.17.3")
+    ],
+    targets: [
+        .target(name: "IceCream", dependencies: ["RealmSwift"], path: "IceCream/Classes"),
+    ]
+)
+
